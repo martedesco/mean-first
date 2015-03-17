9d56087064a25fb4682617d8e444ae891efc1dff
marciotedesco <marcio.tedesco@gmail.com>
2015-03-17 20:14:56 -0300

    Added authentication system with Passport

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose'),
+	crypto = require('crypto'),
 	Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
@@ -6,23 +7,34 @@ var UserSchema = new Schema({
 	lastName: String,
 	email: {
 		type: String,
-		index: true
+		index: true,
+		match: [/.+\@.+\..+/, "Please fill a valid e-mail address"]
 	},
 	username: {
 		type: String,
 		trim: true,
 		unique: true,
-		required: true
+		required: 'Username is required',
+		trim: true
 	},
 	password: {
 		type: String,
 		validate: [
 			function(password) {
-				return password.length >= 6;
+				return password && password.length >= 6;
 			},
 			'Password should be longer'
 		]
 	},
+	salt: {
+		type: String
+	},
+	provider: {
+		type: String,
+		required: 'Provider is required'
+	},
+	providerId: String,
+	providerData: {},
 	role: {
 		type: String,
 		enum: ['Admin', 'Owner', 'User']
@@ -48,7 +60,42 @@ var UserSchema = new Schema({
 	}
 });
 
-mongoose.model('User', UserSchema);
+UserSchema.pre('save', function(next) {
+	if (this.password) {
+		this.salt = new
+		Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+		this.password = this.hashPassword(this.password);
+	}
+	
+	next();
+});
+
+UserSchema.methods.hashPassword = function(password) {
+	return crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('base64');
+};
+
+UserSchema.methods.authenticate = function(password) {
+	return this.password === this.hashPassword(password);
+};
+
+UserSchema.statics.findUniqueUsername = function(username, suffix, callback) {
+	var _this = this;
+	var possibleUsername = username + (suffix || '');
+	
+	_this.findOne({
+		username: possibleUsername
+	}, function(err, user) {
+		if (!err) {
+			if (!user) {
+			callback(possibleUsername);
+			} else {
+				return _this.findUniqueUsername(username, (suffix || 0) + 1, callback);
+			}
+		} else {
+			callback(null);
+		}
+	});
+};
 
 UserSchema.virtual('fullName').get(function() {
 	return this.firstName + ' ' + this.lastName;
@@ -71,3 +118,5 @@ UserSchema.post('save', function(next) {
 });
 
 UserSchema.set('toJSON', { getters: true , virtuals: true});
+
+mongoose.model('User', UserSchema);
