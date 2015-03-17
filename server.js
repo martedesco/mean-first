1fbc5180993bb0e6106e423f135a6533d5dd3d0e
marciotedesco <marcio.tedesco@gmail.com>
2015-03-16 22:19:15 -0300

    horizontal folder structure implemented

--- server.js
+++ server.js
@@ -1,25 +1,12 @@
-var connect = require('connect');
+var express = require('express');
 
-var app = connect();
+var app = express();
 
-var logger = function(req, res, next) {
-	console.log(req.method, req.url);
-	next();
-};
+app.use('/', function(req, res) {
+	res.send('Hello World');
+});
 
-var helloWorld = function(req, res, next) {
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello World');
-};
-
-var goodbyeWorld = function(req, res, next) {
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Goodbye World');
-};
-
-app.use(logger);
-app.use('/hello', helloWorld);
-app.use('/goodbye', goodbyeWorld);
 app.listen(3000);
-
 console.log('Server running at http://localhost:3000/');
+
+module.exports = app;
